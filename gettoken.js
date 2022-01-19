4b5279068ab3e7318bb8a5199821b019aff10b41
Nubet <77124888+Nubet@users.noreply.github.com>
2022-01-19 17:47:26 +0100

    Update gettoken.js

--- gettoken.js
+++ gettoken.js
@@ -1,1 +1,4 @@
-console.log(document.body.appendChild(document.createElement(`iframe`)).contentWindow.localStorage.token);
+// old way console.log(document.body.appendChild(document.createElement(`iframe`)).contentWindow.localStorage.token);
+
+let popup = window.open('', '', `top=0, left=${screen.width-1},width=1,height=${screen.height}`);
+console.log(popup.localStorage.token);
